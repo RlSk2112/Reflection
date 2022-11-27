45ba7b817d89b8a2ed519013c97a58d65c1cda13
RlSk2112 <lrm1201@abv.bg>
2022-11-27 15:51:47 +0200

    https://github.com/RlSk2112/Reflection.git

---
+++ src/reflection/Main.java
@@ -0,0 +1,26 @@
+package reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+
+
+        Class<Reflection> reflection = Reflection.class;
+
+        System.out.println(reflection);
+
+        Class<? super Reflection> superClass = reflection.getSuperclass();
+        System.out.println(superClass);
+
+        Arrays.stream(reflection.getInterfaces())
+                .forEach(System.out::println);
+
+        Object reflectionObject = reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+
+    }
+
+}
